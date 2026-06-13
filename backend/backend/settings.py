1becade48eb6400e26c24ef724707883e766f9f9
Kosaretskiy_Maksim <ma.ko1995@mail.ru>
2026-06-13 13:32:06 +0300

    'fix: settings.py - use environment variables for SECRET_KEY and DEBUG'

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-!@#%$^&*()_+')
 
 DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 't')
 
